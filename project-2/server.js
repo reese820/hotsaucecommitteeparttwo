01f7f2011a3aa60abca654ff5d631fafa7120d82
reese820 <38567375+reese820@users.noreply.github.com>
2018-08-26 22:17:19 -0400

    Add files via upload

--- project-2/server.js
+++ project-2/server.js
@@ -6,25 +6,30 @@ const PORT = process.env.NODE_PORT || 3000;
 
 app.use(express.static("public"));
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(bodyParser.json());
 
-app.engine("handlebars", expressHandlebars({ defaultLayout: "main"}));
+app.engine("handlebars", expressHandlebars({
+  defaultLayout: "main"
+}));
 app.set("view engine", "handlebars");
 //app.use(routes);
 app.get('/', function (req, res) {
-  res.sendFile(path.join(__dirname + '/public/index.html'));
+  res.sendFile(path.join(__dirname + '/public/inputs.html'));
 });
-app.post('/index', function(req, res){
-  res.upgrading
-})
+app.post('/index', function (req, res) {
+  res.sendFile(path.join(__dirname + '/public/inputs.html'));
+});
 app.get('/model', function (req, res) {
-  //res.render('./home', {stocks:"stock symbol,company name, CUSIP"});
+  res.render('../model', {
+    stocks: "stock.symbol,stock.shortName, stock.regularMarketPrice, stock.trailingAnnualDividendRate"
+  });
   res.sendFile(path.join(__dirname + '/public/model.html'));
- });
+});
 
-require("./routes/api-routes.js")(app) {
-app.listen(PORT, function() {
-    console.log("Application listening on PORT " + PORT);
-    })
-})
+require("./routes/api-routes.js")(app)
+app.listen(PORT, function () {
+  console.log("Application listening on PORT " + PORT);
+});
